b656af13c2cc7b490b3da30dc39f449df0ee635f
ksaskas <43259076+ksaskas@users.noreply.github.com>
2018-09-14 09:05:50 +0530

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,7 @@
+class Movie
+{
+public static void main (String args[])
+{
+System.out.println("5 - 3");
+}
+}
---
+++ Time.java
@@ -0,0 +1,7 @@
+class Time
+{
+public static void main (String args[])
+{
+System.out.println("Now the time is 9:am");
+}
+}
